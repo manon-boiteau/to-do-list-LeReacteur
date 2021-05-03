b6bb6e60f255a04440d8b8c0f15497fab48eba65
Manon Boiteau <manonboiteau@mbp-de-manon.kpn>
2021-05-03 18:53:11 +0200

    css & layout

--- src/App.js
+++ src/App.js
@@ -28,54 +28,81 @@ function App() {
 
   return (
     <div>
-      <ul>
-        {addTask.map((elem, index) => {
-          console.log(elem);
-          console.log(index);
-          return (
-            <li key={index}>
-              <input
-                type="checkbox"
-                id={index}
-                name="task"
-                isCheck={elem.isCheck}
-                onClick={() => {
-                  const checked = [...addTask];
-                  checked[index].isCheck === false
-                    ? (checked[index].isCheck = true)
-                    : (checked[index].isCheck = false);
-                  setAddTask(checked);
-                }}
-              />
-              <label
-                htmlFor={index}
-                style={{
-                  textDecoration: elem.isCheck ? "line-through" : "none",
-                }}
-              >
-                {elem.name}
-              </label>
-              <FontAwesomeIcon
-                icon="trash"
-                onClick={() => {
-                  const removed = [...addTask];
-                  removed.splice(index, 1);
-                  setAddTask(removed);
-                }}
-              />
-            </li>
-          );
-        })}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="new task"
-          value={task}
-          onChange={handletask}
-        />
-        <input type="submit" value="Add task" />
-      </form>
+      <header>
+        <div className="wrapper">
+          <FontAwesomeIcon className="icon-list" icon="list" />
+          <h1>To do List</h1>
+        </div>
+      </header>
+      <main className="wrapper">
+        <ul className="wrapper">
+          {addTask.map((elem, index) => {
+            console.log(elem);
+            console.log(index);
+            return (
+              <li key={index}>
+                <input
+                  type="checkbox"
+                  id={index}
+                  name="task"
+                  isCheck={elem.isCheck}
+                  onClick={() => {
+                    // or onChange
+                    const checked = [...addTask];
+                    checked[index].isCheck === false
+                      ? (checked[index].isCheck = true)
+                      : (checked[index].isCheck = false);
+                    setAddTask(checked);
+                  }}
+                />
+                <label
+                  htmlFor={index}
+                  style={{
+                    textDecoration: elem.isCheck ? "line-through" : "none",
+                  }}
+                >
+                  {elem.name}
+                </label>
+                <FontAwesomeIcon
+                  className="icon-trash"
+                  icon="trash"
+                  onClick={() => {
+                    const removed = [...addTask];
+                    removed.splice(index, 1);
+                    setAddTask(removed);
+                  }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <form className="wrapper" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="new task"
+            value={task}
+            onChange={handletask}
+          />
+          <input type="submit" value="Add task" />
+        </form>
+      </main>
+
+      <footer>
+        <p>
+          2021. By{" "}
+          <a
+            href="https://www.linkedin.com/in/manon-boiteau/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Manon
+          </a>{" "}
+          for{" "}
+          <a href="https://www.lereacteur.io/" target="_blank" rel="noreferrer">
+            Le Réacteur
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
